9790dfba59e0043c89a4af371b24900eb4e64bd1
Álvaro <90500200+02Alvaro@users.noreply.github.com>
2023-10-20 22:49:21 +0200

    feat: :art: colors!

--- src/popup.js
+++ src/popup.js
@@ -1,14 +1,14 @@
 document.addEventListener("DOMContentLoaded", () => {
-    const buttonElements = document.querySelectorAll('.Btn'); 
-    
+    const buttonElements = document.querySelectorAll('.Btn');
+
     const sendMessage = (action) => {
-        chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-            chrome.tabs.sendMessage(tabs[0].id, {action});
+        chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+            chrome.tabs.sendMessage(tabs[0].id, { action });
         });
     };
 
     buttonElements.forEach((buttonElement) => {
-        const action = buttonElement.id; 
+        const action = buttonElement.id;
         buttonElement.addEventListener("click", () => sendMessage(action));
     });
 });
